bddc9e64db1d1001e8f80c891f8602b96a4f0a3c
Apoorva  Nagaraj <apoorva27@ccs.neu.edu>
2017-03-02 01:59:39 -0500

    testing heroku

--- server.js
+++ server.js
@@ -1,35 +1,19 @@
 var express = require('express');
 var app = express();
 
-
+// install, load, and configure body parser module
 var bodyParser = require('body-parser');
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-var passport      = require('passport');
-var cookieParser  = require('cookie-parser');
-var session       = require('express-session');
-
-app.use(session({
-    secret: 'this is the secret',
-    resave: true,
-    saveUninitialized: true
-}));
-app.use(cookieParser());
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-app.set('view engine', 'ejs');
+app.use(bodyParser.json());
 
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
-require ("./test/app.js")(app);
-
-require ("./assignment/app.js")(app);
-//assignment(app);
 
 var port = process.env.PORT || 3000;
+
+require ("./test/app.js")(app);
+var assignment = require("./assignment/app.js");
+assignment(app);
 
 app.listen(port);
