ebc9ac4e6324010f2580bc1030c8880e9acd0ecb
Apoorva  Nagaraj <apoorva27@ccs.neu.edu>
2017-04-08 01:26:02 -0400

    final home page

---
+++ express.js
@@ -0,0 +1,8 @@
+/**
+ * Created by hiresave on 4/6/2017.
+ */
+
+var express = require('express');
+var app = express();
+app.express = express;
+module.exports = app;
--- project/app.js
+++ project/app.js
@@ -3,18 +3,13 @@
  */
 
 module.exports = function (app) {
-
     var connectionString ='mongodb://127.0.0.1:27017/test';
 
     var models = require('./model/models.server')();
-
     // var UserModel = require('./model/user/user.model.server')();
     require("./services/user.service.server") (app,models.UserModel);
     require("./services/attraction.service.server.js") (app,models.AttractionModel);
     require("./services/entry.service.server.js") (app,models.EntryModel);
     require("./services/expert.service.server.js") (app,models.ExpertModel);
 
-
 }
-
-
